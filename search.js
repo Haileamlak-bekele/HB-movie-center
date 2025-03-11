a093a8102fe272f91d338aac0994fd4f236eeb72
Haileamlak-bekele <haileamlakbekele@gmail.com>
2025-03-11 12:42:30 +0300

    logo

--- search.js
+++ search.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     };
   
-    fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=true&language=en-US&page=1`, options)
+    fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`, options)
       .then((res) => res.json())
       .then((data) => {
         if (data.results) {
